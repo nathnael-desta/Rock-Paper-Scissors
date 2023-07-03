e73c73cb1e2e15aee56e37346a6405fbc8d74f49
Nathnael Desta <destanathnaelataro@gmail.com>
2023-07-03 19:46:39 +0300

    add playerSelection and result

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
+    let randomNumber = Math.floor(Math.random() * 3);
     return randomNumber == 0 ? "Rock":
            randomNumber == 1 ? "Paper":
-           "Scissors"
+           "Scissors";
 }
+
+function playRound(playerSelection,computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    
+    return playerSelection == "rock" && computerSelection == "scissors" ||
+    playerSelection == "paper" && computerSelection == "rock" ||
+    playerSelection == "scissors" && computerSelection == "paper"
+    ?`You Win! ${playerSelection} beats ${computerSelection}` :
+    computerSelection == "rock" && playerSelection == "scissors" ||
+    computerSelection == "paper" && playerSelection == "rock" ||
+    computerSelection == "scissors" && playerSelection == "paper"
+    ?`You lose! ${computerSelection} beats ${playerSelection}` :
+    `Draw! ${playerSelection} draws against ${computerSelection}`;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
