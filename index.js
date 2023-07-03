2ac66f11c0ae37e3c19449635c62435b3245a51b
Nathnael Desta <destanathnaelataro@gmail.com>
2023-07-03 18:11:39 +0300

    add computer chice generator

---
+++ index.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    return randomNumber == 0 ? "Rock":
+           randomNumber == 1 ? "Paper":
+           "Scissors"
+}
